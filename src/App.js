e609ac7939564741a723e1ea2e0dbe279e8f0833
Michael Lutz <mikelutz2400@gmail.com>
2023-01-26 12:29:25 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import EarthWeather from "./components/EarthWeather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Hello</header>
+      <EarthWeather />
     </div>
   );
 }
---
+++ src/components/EarthWeather.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EarthWeather() {
+  return <div className="btn btn-secondary">Hello World</div>;
+}
+
+export default EarthWeather;
